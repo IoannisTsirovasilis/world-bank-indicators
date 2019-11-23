5bbe9c1c1730e15e238bab00fce26e034c27f618
itsirovasilis <ioannistsirovasilis@gmail.com>
2019-11-24 01:33:23 +0200

    - calculate best indicators based on maximum means

--- indicators_Banks.py
+++ indicators_Banks.py
@@ -14,18 +14,21 @@ ind_number = 1
 years = list(range(2019, 1959, -1))
 completeness_ratios = {}
 countries_names = ['Switzerland', 'Germany', 'Spain', 'Greece']
+mean_values = []
 a = 1
 for ind_code in indicators:
     res = wb.get_data(ind_code, country=countries, pandas=True)
-    
+
     # completeness
     for cn in countries_names:
         # Get current country's years
         keys = [key for key in res.keys() if key[0] == cn]
         # Calculate the list of empty values
-        empty_values = [res[key] for key in keys if math.isnan(res[key])]
+        empty_values = [res[key] for key in keys if res[key] is not None and math.isnan(res[key])]
         # Completeness ratio is (n_all_values - n_empty_values) / n_all_values * 100
-        ratio = (len(keys) - len(empty_values)) / len(keys) * 100
+        ratio = 0
+        if len(keys) > 0:
+            ratio = (len(keys) - len(empty_values)) / len(keys) * 100
         completeness_ratios["{0} - {1}".format(ind_code, cn)] = ratio
 
     # cohesivity
@@ -35,8 +38,8 @@ for ind_code in indicators:
         # Get current year's (y) keys for every country
         # e.g keys = [('Switzerland', '2019'), ('Germany', '2019'), ('Spain', '2019'), ('Greece', '2019')]
         # if y = 2019
-        keys = [key for key in res.keys() if int(key[1]) == y]
-        indicator_values = [math.isnan(res[key]) for key in keys]
+        keys = [key for key in res.keys() if key[1] == str(y)]
+        indicator_values = [math.isnan(res[key]) for key in keys if res[key] is not None]
 
         # if True is in indicator values it means that we have found at least one "nan" value in a country
         # thus breaking the cohesivity
@@ -48,11 +51,21 @@ for ind_code in indicators:
             indicators_cohesive_set.append(y)
             count += 1
     biggest_cohesive_sets[ind_code] = indicators_cohesive_set
+    
+    non_nan_values = [float(x) for x in res.values if x is not None and not math.isnan(x)]
+    if len(non_nan_values) > 0:
+        mean_values.append((ind_code, sum(non_nan_values) / len(non_nan_values)))
+
+print(mean_values)
+best_ten_mean_values = sorted(mean_values, key=lambda x: x[1], reverse=True)[:10]
 
 for k, v in biggest_cohesive_sets.items():
     print("Indicator: {0} - Biggest Cohesive Set: {1}".format(k, v))
 
 for k, v in completeness_ratios.items():
     print("Indicator - Country: {0} - Completeness Ratio: {1}%".format(k, v))
+
+for k, v in mean_values.items():
+    print("Indicator: {0} - Mean: {1}".format(k, v))
 
 
